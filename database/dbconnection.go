f078d90c85f1798786a8a2059663514be99f5e0d
Sushil Subedi <sushilsubedi32@gmail.com>
2020-02-27 23:21:15 +0545

    databse added

---
+++ database/dbconnection.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"github.com/jinzhu/gorm"
+
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	// _ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+var DB *gorm.DB
+
+func init(){
+	var err error
+	
+	DB, err = gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic(err)
+	}
+}
+func ConnectDB() *gorm.DB {
+	return DB
+}
